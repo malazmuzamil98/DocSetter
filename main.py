d44eed3ce7a42146f1c30a65333a05a43d6d168e
AlWaleed Musa <jako_249@hotmail.com>
2024-01-27 14:25:48 +0200

    update read me + main.py refactoring

--- main.py
+++ main.py
@@ -9,19 +9,17 @@ def main():
 	Start of the program with checking of command line input is correct
 	"""
 
-	if len(sys.argv) != 4:
-		print("Usage: <Script_name> <Module_name> <Function_name> <.txt>")
-		sys.exit("Help: <Script_name> -h or --help (NOT READY YET)")   # <------ maybe will add
+	if len(sys.argv) != 2:
+		print("Usage: <Script_name> <Module_name>")
+		# sys.exit("Help: <Script_name> -h or --help (NOT READY YET)")   # <------ maybe will add
 
 	# Validate the function name passed from the command line
-	function = sys.argv[2]
 	module = sys.argv[1]
-	txt = sys.argv[3]
 
-	if function_module_validate(function, module, txt):
-		function_name = function
-		module_name = module[:-3]
-		txt_name = txt
+	if function_module_validate(module):
+		function_name = module[2:-3]
+		module_name = module
+		txt_name = re.sub(r"\.py", ".txt", module)
 
 		# create a copy of the blueprint using the function name passed and module name
 		ready_to_write = make_doc(function_name, module_name)
@@ -36,7 +34,7 @@ def write_doc(new_lines, txt_filename):
 
 	Args:
 		new_lines (list): list of new fixed lines ready to write
-		function_name (str): function name to use
+		txt_filename (str): text file to write to
 
 	Return:
 		Nothing, Will Create a new "tests" dir if not there and a new .txt ready to edit
@@ -72,53 +70,34 @@ def make_doc(function_name, module_name):
 		lines = file.readlines()
 		for line in lines:
 			line = re.sub(r"<function_name>", function_name, line)
-			line = re.sub(r"<file_name>", module_name, line)
+			line = re.sub(r"<file_name>", module_name[:-3], line)
 			new_lines.append(line)
 	return new_lines
 
 
-def function_module_validate(function, module, txt):
+def function_module_validate(module):
 	"""
 	a function that take two strings from command line and check if its a valid
 	function name and a valid module name
 
 	Args:
-		function (str): string passed to validate function name
 		module (str): string passed to validate module name
-		txt_filename (str): string passed to validate txt output file
 
 	Return:
-		True if they are valid names, False if 1 or more are not valid
+		True if its a valid name
 
 	Raise:
-		ValueError: if one or all names are inValid
+		ValueError: if names is inValid
 	"""
 	module_ok = 0
-	function_ok = 0
-	txt_ok = 0
-
-	if function:
-		function_name = re.search(r"^[a-zA-Z_]\w*$", function)
-		if function_name:
-			function_ok = 1
-		else:
-			raise ValueError("Please provide a Valid Function name")
-	
 	if module:
 		module_name = re.search(r"\.py$", module)
 		if module_name:
 			module_ok = 1
 		else:
 			raise ValueError("Please provide a Valid Module name")
-		
-	if txt:
-		txt_file = re.search(r"\.txt$", txt)
-		if txt_file:
-			txt_ok = 1
-		else:
-			raise ValueError("Please provide a Valid Txt file name")
 
-	if module_ok and function_ok and txt_ok:
+	if module_ok:
 		return True
 
 
